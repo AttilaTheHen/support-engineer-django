2d957d46effd145f96189fc67469a9c7c901d3b2
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2024-04-22 17:55:13 -0700

    🎉 Remove my solution

--- worksample/worksample/apps/core/views.py
+++ worksample/worksample/apps/core/views.py
@@ -1,26 +1,10 @@
-import os
-import requests
-
-from dotenv import load_dotenv
 from django.views.generic import TemplateView
 
-load_dotenv()
 
 class HomepageView(TemplateView):
     template_name = "homepage.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        app_name = os.getenv("FLY_APP_NAME")
-        machine_id = os.getenv("FLY_ALLOC_ID")
-        api_token = os.getenv("FLY_API_TOKEN")
-        url = f"https://api.machines.dev/v1/apps/{app_name}/machines/{machine_id}"
-        headers = {
-            "Authorization": f"Bearer {api_token}",
-            "Accept": "application/json"
-        }
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            context["machines"] = response.json()
-            
+
         return context
